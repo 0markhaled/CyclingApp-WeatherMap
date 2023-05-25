560c36f96d3cb6d0fd003ce28617b74172d1bb14
joshuadwalker11 <joshuadwalker11@gmail.com>
2023-05-25 11:55:45 -0400

    navigation

---
+++ frontendWebpackHbs/src/js/json/pages.js
@@ -0,0 +1,25 @@
+export default {
+    'siteInfo': { 'title': "Site title" },
+    'pages': [
+        {
+            'name': 'Home',
+            'headertext': 'Home page',
+        },
+
+        {
+            'name': 'Map',
+            'headertext': "This is the map",
+        },
+
+        {
+            'name': 'Info',
+            'headertext': "About Us",
+        },
+
+        {
+            'name': 'Contact Us',
+            'headertext': "Contact",
+        }
+    ]
+
+}
